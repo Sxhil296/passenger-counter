1d7d36106ed73fa0cd695ed6f283e7ec8cc2126d
Sahil <leviack629@gmail.com>
2022-09-18 11:47:39 +0530

    Comlete

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
-// document.getElementById("date-el").innerHTML = Date().toLocaleDateString();
+var today = new Date();
+document.getElementById("date-el").textContent  += today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+document.getElementById("time-el").textContent += today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
+
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let clearPre = document.getElementById("save-el")
+
 
 function increment() {
     count = count + 1
@@ -10,5 +16,14 @@ function increment() {
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + ", "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    // saveEl.style.border = "1px solid white";
+}
+
+
+function reset() {
+    saveEl.textContent = "Previous entries: "
 }
